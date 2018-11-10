3e198421717daf4f5ec3c1f01da5d51eade585f0
Jose Fco. Catala <catala@armentechnology.com>
2018-11-10 09:13:02 +0000

    Added new files to create keyboard class. wip

--- src/main.cpp
+++ src/main.cpp
@@ -20,20 +20,16 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Arduino.h>
+#include "keyboardHelper.h"
 
 #define __DEBUG
 
 #define RST_PIN         9          // Configurable, see typical pin layout above
 #define SS_PIN          10         // Configurable, see typical pin layout above
 
-//#define KEY_LEFT_SHIFT  0x02
-#define KEY_LEFT_SHIFT  0xE1
-//#define KEY_LEFT_CTRL   0x01
-#define KEY_LEFT_CTRL   0xE0
-#define KEY_POWER       0x66
-#define KEY_ENTER       0x28
-#define KEY_1           0x1E
-#define KEY_3           0x20
+#define MY_CARD         20435
+#define MY_TAG          29468
+
 
 int trigPin = 6;    // Trigger
 int echoPin = 7;    // Echo
@@ -72,13 +68,14 @@ void setup()
 	while (!Serial);		    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
 	SPI.begin();			      // Init SPI bus
 	mfrc522.PCD_Init();		  // Init MFRC522
- 
+
 //  Show details of PCD - MFRC522 Card Reader details
 //	mfrc522.PCD_DumpVersionToSerial();	
 
    //Define inputs and outputs
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
 }
 
 void loop() 
@@ -101,10 +98,7 @@ void loop()
   // Convert the time into a distance
   cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
   inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
-  /*}*/
-
-
-    
+  
   if((inches > 80) && ( millis() - timer > 5000)  && (locked == false))//away for more than 5 sec
   {
     #ifdef __DEBUG
@@ -120,7 +114,11 @@ void loop()
     releaseKey();
     locked = true;
   }
+  /*}*/
 
+  /*!
+  *@note MFRC522 Reader
+  *{*/
 	// Look for new cards
 	if ( ! mfrc522.PICC_IsNewCardPresent()) {
 		return;
@@ -139,27 +137,18 @@ void loop()
   unsigned long uid = getID();
 
   //This means my card
-  if(uid == 20435 || uid == 29468 && locked == true)
+  if(uid == MY_CARD || uid == MY_TAG && locked == true)
   {
     //Type enter first to open the login screen
     //and wait for a bit
-    buf[2] = KEY_LEFT_CTRL;
-    Serial.write(buf, 8); // Send keypress
-    releaseKey();
-    delay(300);
 
     //Now type the password
-    buf[2] = KEY_1;    // 1
-    Serial.write(buf, 8); // Send keypress
-    releaseKey();
-    delay(150);
-    buf[2] = KEY_3;    // 3
-    Serial.write(buf, 8); // Send keypress
-    releaseKey();
-    delay(110);
-    buf[2] = KEY_ENTER;    // enter
-    Serial.write(buf, 8); // Send keypress
-    releaseKey();
+    delay(200);
+    //Keyboard.print("13");
+    delay(500);
+    //Keyboard.press(KEY_RETURN);
+    delay(200);
+    //Keyboard.releaseAll();
 
     //Reinitialize the counter for the proximity device
     timer = millis();
@@ -170,4 +159,5 @@ void loop()
       Serial.println("Unlocked");
     #endif
   }
+  /*}*/
 }
